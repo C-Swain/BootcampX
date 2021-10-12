cb7ee6e28e31e0de5b24661f16bac98e7f62a662
C-Swain <catherinemexicoswain@gmail.com>
2021-10-12 19:50:03 +0000

    done queries 3

---
+++ sub1/select_from_sub_table.sql
@@ -0,0 +1,7 @@
+SELECT AVG(total_students) as average_students
+FROM (
+  SELECT COUNT(students) as total_students, cohorts.name as cohort_name
+  FROM students
+  JOIN cohorts on cohorts.id = cohort_id
+  GROUP BY cohorts.name
+) as totals_table;
