ea54c5e2065d5c791a8a2ced8768c2f38cdd394b
C-Swain <catherinemexicoswain@gmail.com>
2021-10-12 13:55:35 +0000

    halfway through group by

---
+++ queries_3_by_group/busy_days.sql
@@ -0,0 +1,5 @@
+SELECT day, Count(*) as total_assignments 
+FROM assignments
+GROUP BY day
+HAVING COUNT(assignments.*) >= 10
+ORDER BY day;
---
+++ walkthrough/total_submissions.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(assignment_submissions.*) AS total_submissions
+FROM assignment_submissions;
